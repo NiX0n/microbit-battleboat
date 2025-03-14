600de0a7cb026896662b51446710ce9965b8c76b
NiX0n <BiggoronSword+GitHub@gmail.com>
2025-03-14 14:36:24 -0400

    Added: JSDoc and other meaningful documentation

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
 /**
  * https://github.com/NiX0n/microbit-battleboat
+ * 
  * @TODO
  * - Add ship placment mode/UX
  * - Add bomb receiver mode/UX
  * - Add blink hold on fire sequence, etc
+ * - Add user-switchable nPlayers
+ */
+
+/**
+ * Start New Game
  */
 function newGame () {
     newLedBuffer()
@@ -12,6 +18,7 @@ function newGame () {
     nPlayers = 2
     if(nPlayers > 1)
     {
+        radio.setTransmitSerialNumber(true)
         radio.setGroup(RADIO_GROUP)
         // Handshake
         radio.sendString('null')
@@ -27,8 +34,11 @@ function newGame () {
 
 }
 
+/**
+ * Construct ledBuffer boolean[5][5] matrix.
+ * We use a separate LED output buffer for smooth/flickerless rendering.
+ */
 function newLedBuffer() {
-    // ledBuffer is a boolean[5][5] matrix.
     ledBuffer = []
     // We're initializing using for loops because of limitations of the JavaScript engine (i.e no support for Array constructor).
     for (let x1 = 0; x1 <= LED_BUFFER_WIDTH - 1; x1++) {
@@ -39,7 +49,9 @@ function newLedBuffer() {
     }
 }
 
-// We use a separate LED output buffer for smooth/flickerless rendering.
+/**
+ * Render ledBuffer.
+ */
 function renderLedBuffer () {
     for (let x = 0; x <= ledBuffer.length - 1; x++) {
         for (let y = 0; y <= ledBuffer[x].length - 1; y++) {
@@ -51,6 +63,7 @@ function renderLedBuffer () {
         }
     }
 }
+
 input.onButtonPressed(Button.AB, function () {
     radio.sendString(JSON.stringify({c:cursor}))
 
@@ -100,30 +113,57 @@ input.onButtonPressed(Button.B, function () {
     moveCursor(true)
 })
 
+/**
+ * Move cusor right or down
+ * @param rightDown
+ *      false: right,
+ *      true: down
+ */
 function moveCursor (rightDown: boolean) {
     ledBuffer[cursor[0]][cursor[1]] = false
     cursor[rightDown ? 0 : 1] = (cursor[rightDown ? 0 : 1] + 1) % (rightDown ? ledBuffer.length : ledBuffer[0].length)
 }
 
+/**
+ * BLINK! BLINK! BLINK!
+ */
 function blinkCursor()
 {
     ledBuffer[cursor[0]][cursor[1]] = !ledBuffer[cursor[0]][cursor[1]]
 }
 
+//
+// Declare pseudo-constants
+//
+
 let LED_BUFFER_WIDTH = 5
 let LED_BUFFER_HEIGHT = 5
 let RADIO_GROUP = 3
 let SERIAL_NUMBER: number = control.deviceSerialNumber()
+let LOOP_DELAY = 500
+
+
+//
+// Declare dynamic variables
+//
+
 let cursor: number[] = []
 let ship: number[] = []
 let ledBuffer: boolean[][] = []
 let nPlayers: number = 0
-radio.setTransmitSerialNumber(true)
+
+//
+// Initialize
+//
 newGame()
 
+
+/**
+ * Loop mostly drives LEDs
+ */
 basic.forever(function () {
     blinkCursor()
     renderLedBuffer()
     // This controls the speed of the game
-    basic.pause(500)
+    basic.pause(LOOP_DELAY)
 })
